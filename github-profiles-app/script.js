8394abe0d4c28c91604d704b7f7ddf1779c5753b
Dmytro Zhuchenko <mixakievchanen@gmail.com>
2022-11-19 13:07:29 +0200

    ,

--- github-profiles-app/script.js
+++ github-profiles-app/script.js
@@ -3,6 +3,8 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
+getUser("MikeKorotych");
+
 async function getUser(user) {
   try {
     const response = await fetch(AIPURL + user);
@@ -18,18 +20,33 @@ async function getUser(user) {
 function createUserCard(user) {
   const cardHTML = `
   <div class="card">
-    <div>
+    <div class='img-container'>
       <img src='${user.avatar_url}' alt='${user.name}' class='avatar'/>
     </div>
     
-    <div>
+    <div class='user-info'>
       <h2>${user.name}</h2>
       <p>${user.bio ? user.bio : "do not have bio💔"}</p>
     
       <ul class='info'>
-        <li>${user.followers}</li>    
-        <li>${user.following}</li>    
-        <li>${user.public_repos}</li>    
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+          ${user.followers}
+        </li>    
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+          </svg>
+          ${user.following}
+        </li>    
+        <li>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+</svg>
+${user.public_repos}</li>    
       </ul>
     </div>
   </div>
