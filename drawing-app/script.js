dc64e9c8d1ae7f9094e51c4a157aa3b3d53e493c
Dmytro Zhuchenko <mixakievchanen@gmail.com>
2022-11-20 21:44:02 +0200

    done

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -6,12 +6,13 @@ const decreaseBtn = document.getElementById("decrease");
 
 const displaySizeEl = document.querySelector("span");
 const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
 
 let size = 10;
-let x = 50;
-let y = 50;
 let isPressed = false;
 let color = "black";
+let x = undefined;
+let y = undefined;
 
 function dr(e) {
   if (isPressed) {
@@ -23,13 +24,27 @@ function dr(e) {
 
 canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
   dr(e);
 });
 canvas.addEventListener("mouseup", (e) => {
   isPressed = false;
+
+  x = undefined;
+  y = undefined;
 });
 canvas.addEventListener("mousemove", (e) => {
-  dr(e);
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+  }
 });
 
 function drawCircle(x, y) {
@@ -37,6 +52,15 @@ function drawCircle(x, y) {
   ctx.arc(x, y, size, 0, 2 * Math.PI);
   ctx.fillStyle = color;
   ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
 }
 
 increaseBtn.addEventListener("click", (e) => {
@@ -50,4 +74,8 @@ decreaseBtn.addEventListener("click", (e) => {
 
 colorEl.addEventListener("change", (e) => {
   color = e.target.value;
+});
+
+clearEl.addEventListener("click", (e) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
